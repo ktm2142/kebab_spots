ae7f1cce0bcf04cc4136ac4e08ae4b60e52e15e5
Volodymyr Oliinyk <ktm2142@gmail.com>
2025-12-04 17:54:13 +0000

    created KebabSpots app, its model and serializer.

---
+++ backend/kebab_spots_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.gis.db import models as gis_models
+from config_app.settings import AUTH_USER_MODEL
+
+
+class KebabSpot(models.Model):
+    location = gis_models.PointField()
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='spots')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
