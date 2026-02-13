cea4fac05db40da6e4cb0b0d82e3c458c83f9f8f
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-02-13 16:13:44 +0000

    changed url of frontend app railway

--- backend/config_app/settings.py
+++ backend/config_app/settings.py
@@ -143,7 +143,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'https://kebab-spots.up.railway.app/'
+    'https://kebab-spots.up.railway.app'
 ]
 
 REST_FRAMEWORK = {
