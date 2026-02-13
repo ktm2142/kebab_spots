97d262bb9cf83ce5bfc1ec2f431e943e14d9b03a
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-02-13 16:52:50 +0000

    fix Leaflet icon

--- frontend/src/components/map/BaseMap.jsx
+++ frontend/src/components/map/BaseMap.jsx
@@ -1,6 +1,17 @@
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useMemo } from "react";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Icon fix Leaflet after building Vite
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconAnchor: [12, 41],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 /**
  * ChangeMapLocation - A component that updates the map view when center prop changes.
