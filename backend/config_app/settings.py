49c699a7019e5afb38ff77a7d8d65845ef9337bc
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-02-14 16:14:47 +0000

    WHITENOISE_MANIFEST_STRICT

--- backend/config_app/settings.py
+++ backend/config_app/settings.py
@@ -160,6 +160,8 @@ STORAGES = {
 STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
+WHITENOISE_MANIFEST_STRICT = False
+
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
