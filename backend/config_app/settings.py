179b8f8fa69b2f4ad8a16f6342c03663afd933ea
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-02-14 15:37:12 +0000

    add STORAGES in settings and deleted DEFAULT_FILE_STORAGE

--- backend/config_app/settings.py
+++ backend/config_app/settings.py
@@ -145,7 +145,14 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
