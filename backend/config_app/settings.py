3c54d7bb164ba884f58271093d7afa248884ba03
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-02-15 15:38:05 +0000

    STATICFILES_FINDERS and STATICFILES_DIRS add

--- backend/config_app/settings.py
+++ backend/config_app/settings.py
@@ -134,6 +134,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = []
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
